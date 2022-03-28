1710c9f4d5d61804f4c900077bf85ae573c952eb
zumb <zumb@zumb.zumb>
2022-03-28 20:33:39 -0300

    finish select quantiti

--- applist/src/data/product.ts
+++ applist/src/data/product.ts
@@ -4,7 +4,13 @@ export default {
   description: `Cerveja deliciosa feita de mandioca.`,
   image: 'https://imagekank.s3.amazonaws.com/magnifica_269ml.png',
   price: 99.98,
-  quantitiSelector: 3,
+  quantitiSelector: [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'
+  ],
   totalPrice: 120.06,
   button:2
 };
--- applist/src/screens/ProductScreen/index.tsx
+++ applist/src/screens/ProductScreen/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Image } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import styles from "./styles";
 import product from "../../data/product";
 
 const ProductScreen = () => {
+    const [selectQuantiti, setSelectQuantiti ] = useState(product.quantitiSelector[0]);
     return (
         <View>
             <Text style={styles.name}>{product.name}</Text>
@@ -24,16 +25,14 @@ const ProductScreen = () => {
 
             {/* Quantiti Selector */}
             <Text style={styles.quantity}>{product.quantitiSelector}</Text>
-                <Picker>
-                    <Picker.Item label = "1" value={1} />
-                    <Picker.Item label = "2" value={2} />
-                    <Picker.Item label = "3" value={3} />
-                    <Picker.Item label = "5" value={4} />
-                    <Picker.Item label = "6" value={5} />
-                    <Picker.Item label = "7" value={6} />
-                    <Picker.Item label = "8" value={7} />
-                    <Picker.Item label = "9" value={8} />
-                    <Picker.Item label = "10" value={9} />
+                <Picker
+                  selectedValue={selectQuantiti}
+                  onValueChange={( itemValue ) => 
+                  setSelectQuantiti( itemValue )           
+                }>
+                    { product.quantitiSelector.map(option =>(
+                    <Picker.Item label = {option} value={option} />
+                    ))}
                 </Picker>
             {/* totalPrice */}
 
